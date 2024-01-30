497183d680485b239096c1808dd27f0c02626d90
Fabricio Da Silva Ribeiro <102611358+Fabricio1408@users.noreply.github.com>
2024-01-29 23:36:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-//js somente na aula 4
+
+const greetingElement = document.getElementById("greeting");
+
+const currentHour = new Date().getHours();
+
+ if (currentHour >= 5 && currentHour < 12) {
+   greetingElement.textContent = "Bom dia";
+ } else if (currentHour >= 12 && currentHour < 18) {
+   greetingElement.textContent = "Boa tarde";
+ } else {
+   greetingElement.textContent = "Boa noite";
+ }
+
+
+
+const container = document.querySelector(".offer__list-item");
+
+const observer = new ResizeObserver(() => {  
+  const containerWidth = container.offsetWidth;
+  const numColumns = Math.floor(containerWidth / 200); 
+
+  container.style.gridTemplateColumns = `repeat(${numColumns}, minmax(200px, 1fr))`;
+  console.log({ container });
+  console.log({ numColumns });
+});
+observer.observe(container);
+
+function fechar(){
+  let closed = document.getElementsByClassName('disclaimer-premium')
+
+  closed.style.display = 'none'
+}
